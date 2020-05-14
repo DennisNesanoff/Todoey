45fa5d02eb5dff887884190eac5c074954821a96
Dennis <nndenninn@gmail.com>
2020-05-14 13:39:26 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Dennis Nesanoff on 14.05.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
